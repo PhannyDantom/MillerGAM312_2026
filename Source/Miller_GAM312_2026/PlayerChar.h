14276b795dd311e7f3727ee4c664b9ff65b59513
Danny <amanda.miller43@snhu.edu>
2026-01-17 22:38:17 -0600

    Stamina

--- Source/Miller_GAM312_2026/PlayerChar.h
+++ Source/Miller_GAM312_2026/PlayerChar.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
 #include "Resource_M.h"
+#include "Kismet/GameplayStatics.h"
 #include "PlayerChar.generated.h"
 
 UCLASS()
@@ -87,6 +88,9 @@ public:
 
 	UPROPERTY(EditAnywhere, Category = "Resources")
 		TArray<FString> ResourcesNameArray;
+
+	UPROPERTY(EditAnywhere, Category = "HitMarker")
+		UMaterialInterface* hitDecal;
 
 	UFUNCTION()
 		void GiveResource(float amount, FString resourceType);
